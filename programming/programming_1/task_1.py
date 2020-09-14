7363b995b39716bafb1121b792e49b3429e6319e
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2020-09-14 16:53:19 +0300

    added function and validation

--- programming/programming_1/task_1.py
+++ programming/programming_1/task_1.py
@@ -1,7 +1,15 @@
 import random
 
 
+class LessThanA(Exception):
+    pass
+
+
 def gcd(num1, num2):
+    if num1 < 0:
+        num1 = -num1
+    if num2 < 0:
+        num2 = -num2
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2
@@ -10,12 +18,27 @@ def gcd(num1, num2):
     return num1 + num2
 
 
-N = int(input("Enter N: "))
-a = int(input("Enter a:"))
-b = int(input("Enter b:"))
-sequence = [random.randint(a, b) for i in range(N)]
+def generate_sequence(a1, b1, n):
+    s = [random.randint(a1, b1) for i in range(n)]
+    return s
+
+
+while True:
+    try:
+        N = int(input("Enter N: "))
+        a = int(input("Enter a: "))
+        b = int(input("Enter b: "))
+        if b < a:
+            raise LessThanA
+    except ValueError:
+        print("It's not a number")
+    except LessThanA:
+        print("b should be bigger than a")
+    else:
+        break
+sequence = generate_sequence(a, b, N)
 print("Sequence:", *sequence)
-for i in range(0, N):
+for i in range(0, N - 1):
     for j in range(i + 1, N):
         if i != j:
             print("GCD of", sequence[i], "and", sequence[j], "is", gcd(sequence[i], sequence[j]))
