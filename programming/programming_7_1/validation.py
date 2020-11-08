2c2acf2dfea098060cd951cca52c009f3e81448b
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2020-11-08 12:07:46 +0200

    added files to programming_7_1

---
+++ programming/programming_7_1/validation.py
@@ -0,0 +1,21 @@
+import os
+
+
+def validate_file_input():
+    while True:
+        file = input('Input name of your file: ')
+        try:
+            if not os.path.isfile(file):
+                raise FileExistsError
+        except FileExistsError:
+            print('File not exists, try again')
+        else:
+            return file
+
+
+def file_exist(file):
+    try:
+        if not os.path.isfile(file):
+            raise FileExistsError
+    except FileExistsError:
+        print('File not exists')
