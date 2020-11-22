9fc95e66beb85b4372d2604338f8ee2f5bcc1e93
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2020-11-22 23:28:29 +0200

    some change

--- programming/programming_9/mysite/meetings/views.py
+++ programming/programming_9/mysite/meetings/views.py
@@ -1,12 +1,17 @@
 from rest_framework import generics, pagination
+from rest_framework.pagination import PageNumberPagination
+
 from .serializers import OnlineMeetingDetailSerializer, MeetingsListSerializer
 from .models import OnlineMeeting
-from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.filters import OrderingFilter, SearchFilter
 
 
-class CustomPagination(pagination.PageNumberPagination):
-    page_size_query_param = 'limit'
+class PageNumberAsLimitOffset(PageNumberPagination):
+    page_query_param = "offset"
+    page_size_query_param = "limit"
+    page_size = 5
+    max_page_size = 100
 
 
 class MeetingsListView(generics.ListAPIView, generics.CreateAPIView):
@@ -16,7 +21,8 @@ class MeetingsListView(generics.ListAPIView, generics.CreateAPIView):
     search_fields = ['date', 'end_time', 'id', 'meeting_url', 'owner_first_name', 'owner_last_name',
                      'participant_first_name', 'participant_last_name', 'start_time']
     ordering_fields = '__all__'
-    pagination_class = CustomPagination
+    pagination_class = PageNumberAsLimitOffset
+
 
 class OnlineMeetingDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OnlineMeetingDetailSerializer
