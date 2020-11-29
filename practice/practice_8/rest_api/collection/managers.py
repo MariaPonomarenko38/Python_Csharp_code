86228e0803aafe7f42cf6fb20f107770acafbbf4
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2020-11-29 11:47:00 +0200

    minor changes

--- practice/practice_8/rest_api/collection/managers.py
+++ practice/practice_8/rest_api/collection/managers.py
@@ -2,20 +2,10 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    """
-    Django требует, чтобы пользовательские `User`
-    определяли свой собственный класс Manager.
-    Унаследовав от BaseUserManager, мы получаем много кода,
-    используемого Django для создания `User`.
-
-    Все, что нам нужно сделать, это переопределить функцию
-    `create_user`, которую мы будем использовать
-    для создания объектов `User`.
-    """
 
     def _create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Данный адрес электронной почты должен быть установлен')
+            raise ValueError('Email should be set')
 
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
@@ -25,24 +15,16 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        """
-        Создает и возвращает `User` с адресом электронной почты,
-        именем пользователя и паролем.
-        """
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
 
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Создает и возвращает пользователя с правами
-        суперпользователя (администратора).
-        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+            raise ValueError('Superuser should have is_superuser=True.')
 
         return self._create_user(email, password, **extra_fields)
