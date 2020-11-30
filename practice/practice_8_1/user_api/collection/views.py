6d415385b73197739dffd4704ba3295ea43ee543
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2020-11-30 10:11:01 +0200

    added pagination

--- practice/practice_8_1/user_api/collection/views.py
+++ practice/practice_8_1/user_api/collection/views.py
@@ -3,6 +3,14 @@ from .serializers import OnlineMeetingDetailSerializer
 from .models import OnlineMeeting, User
 from rest_framework.filters import OrderingFilter, SearchFilter
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.pagination import PageNumberPagination
+
+
+class PageNumberAsLimitOffset(PageNumberPagination):
+    page_query_param = "offset"
+    page_size_query_param = "limit"
+    page_size = 5
+    max_page_size = 100
 
 
 class MeetingsListView(generics.ListAPIView, generics.CreateAPIView):
@@ -12,6 +20,7 @@ class MeetingsListView(generics.ListAPIView, generics.CreateAPIView):
                      'participant_first_name', 'participant_last_name', 'start_time']
     ordering_fields = '__all__'
     permission_classes = (IsOwnerOrReadOnly,)
+    pagination_class = PageNumberAsLimitOffset
 
     def get_queryset(self):
         return OnlineMeeting.objects.filter(user=self.request.user)
