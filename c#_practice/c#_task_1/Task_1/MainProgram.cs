365938f0fbc1ea9f28f9c6b12575049491d841a2
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2021-02-10 22:44:29 +0200

    added files to task_1

---
+++ c#_practice/c#_task_1/Task_1/MainProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
