a442c561103b8858537fe8a769d6149f11682ce3
MariaPonomarenko38 <mashaponomarenko2002@gmail.com>
2021-02-11 18:52:05 +0200

    added files

--- c#_practice/c#_task_1/Task_1/MainProgram.cs
+++ c#_practice/c#_task_1/Task_1/MainProgram.cs
@@ -2,15 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Task_1
 {
+
     class MainProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            OnlineMeeting a = new OnlineMeeting("4;29.8.2021;08:09;20:25;http://aoom/49839gk483;Denver Maddox;Austin Bradley");
+            a.Set("id", "8");
+            Console.WriteLine(a);
+            Collection ls = new Collection();
+            Console.WriteLine("Write file name:");
+            string file_name = Console.ReadLine();
+            file_name = "C:/code/practice_uni/c#_practice/c#_task_1/Task_1/" + file_name;
+            if (Input.ValidateFileName(file_name) == false)
+            {
+                Console.WriteLine("No such file");
+                return;
+            }
+            ls.FillCollectionFile(file_name);
+            while (true)
+            {
+                Console.WriteLine(@"Input number of option:
+    1. Search meetings with value
+    2. Sort by parametr
+    3. Delete meeting by ID
+    4. Add meeting
+    5. Edit meeting by ID
+    6. See all meetings
+    7. Exit\n");
+                string num = Console.ReadLine();
+                string val = "";
+                if (Int32.Parse(num) >= 1 && Int32.Parse(num) < 6)
+                {
+                    val = Console.ReadLine();
+                }
+                if (num == "1")
+                {
+                    ls.Search(val);
+                }
+                else if (num == "2")
+                {
+                    ;
+                    ls.Sort(val);
+                }
+                else if (num == "3")
+                {
+                    ls.Remove(val, file_name);
+                }
+                else if (num == "4")
+                {
+                    ls.AddTo(val, file_name);
+                }
+                else if (num == "5")
+                {
+                    Console.WriteLine("Type id:");
+                    string id = Console.ReadLine();
+                    ls.Edit(id, val, file_name);
+                }
+                else if (num == "6")
+                {
+                    ls.Print();
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
---
+++ c#_practice/c#_task_1/Task_1/OnlineMeeting.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class OnlineMeeting
+    {
+        string id;
+        string date;
+        string start_time;
+        string end_time;
+        string meeting_url;
+        string owner;
+        string participant;
+
+        public OnlineMeeting(string s)
+        {
+            String[] l = s.Split(';');
+            id = l[0];
+            date = l[1];
+            start_time = l[2];
+            end_time = l[3];
+            meeting_url = l[4];
+            owner = l[5];
+            participant = l[6];
+        }
+        public List<object> GetFields()
+        {
+            var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            List<object> listValues = this.GetType().GetFields(bindingFlags).Select(field => field.GetValue(this)).ToList();
+            return listValues;
+        }
+        public string Get(string s)
+        {
+            string[] fields = { "id", "date", "start_time", "end_time", "meeting_url", "owner", "participant" };
+            int i = 0;
+            foreach (var item in GetFields())
+            {
+                if (fields[i] == s)
+                {
+                    return Convert.ToString(item);
+                }
+                i++;
+            }
+            return "No such field";
+        }
+        public void Set(string s, string val)
+        {
+            Type obj = typeof(OnlineMeeting);
+            var bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            obj.GetField(s, bindingFlags).SetValue(this, val);
+        }
+        public bool search(string s)
+        {
+            foreach (var item in GetFields())
+            {
+                string m = Convert.ToString(item);
+                if (m.Contains(s))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override String ToString()
+        {
+            string res = "";
+            string[] fields = { "id", "date", "start_time", "end_time", "meeting_url", "owner", "participant" };
+            int i = 0;
+            foreach (var item in GetFields())
+            {
+                res += fields[i] + ": " + item + '\n';
+                i++;
+            }
+            return res;
+        }
+        public string FileFormat()
+        {
+            string res = "";
+            foreach (var item in GetFields())
+            {
+                res += item + ";";
+            }
+            res = res.Substring(0, res.Length - 1);
+            return res;
+        }
+    }
+}
