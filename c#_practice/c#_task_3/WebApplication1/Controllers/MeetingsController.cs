3f974df52fc52d345017054b85c0946200e7814c
tpMvDdxC <mashaponomarenko2002@gmail.com>
2021-03-09 15:43:32 +0200

    fixes

--- c#_practice/c#_task_3/WebApplication1/Controllers/MeetingsController.cs
+++ c#_practice/c#_task_3/WebApplication1/Controllers/MeetingsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebApplication1.DataAccess;
 using WebApplication1.Models;
 using LightQuery;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication2.Controllers
 {
